545401fa7d7caeddee4b7a6e8187a597d371d5fb
PRISCILA ALVES <75861909+priscilaalvessilva@users.noreply.github.com>
2025-01-21 07:46:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log ('Isso ai! Você descobriu o número secreto');
+}
+else { 
+    alert(`Que pena! O número secreto é ${numeroSecreto}`);
+}
+    
+
